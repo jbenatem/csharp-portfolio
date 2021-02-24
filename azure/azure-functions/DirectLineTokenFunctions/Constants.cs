cbddcbad4699014ded5ec7e14a00849eb6e9e898
jbenatem <jbenatem@gmail.com>
2021-02-23 19:05:09 -0500

    http trigger azure functions sample added

---
+++ azure/azure-functions/DirectLineTokenFunctions/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace DirectLineTokenGeneration
+{
+    public static class Constants
+    {
+        public const string DIRECT_LINE_SECRET = "INSERT_BOT_SERVICE_DIRECT_LINE_SECRET";
+
+        public const string URL_GENERATE_TOKEN = "https://directline.botframework.com/v3/directline/tokens/generate";
+        public const string URL_REFRESH_TOKEN = "https://directline.botframework.com/v3/directline/tokens/refresh";
+
+        public const string APPLICATION_JSON = "application/json";
+    }
+}
---
+++ azure/azure-functions/DirectLineTokenFunctions/TokenServiceFunction.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using DirectLineTokenGeneration.DirectLineAPI;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace DirectLineTokenGeneration
+{
+    public static class TokenServiceFunction
+    {
+        [FunctionName("TokenGenerationFunction")]
+        public static async Task<Response> Generate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            Response response = await GenerateTokenAsync();
+
+            return response;
+        }
+
+        [FunctionName("TokenRefreshFunction")]
+        public static async Task<Response> Refresh(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string token = req.Query["token"];
+            string requestBody = String.Empty;
+            using (StreamReader streamReader = new StreamReader(req.Body))
+            {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            token = token ?? data?.token;
+
+            Response response = await RefreshTokenAsync(token);
+
+            return response;
+        }
+
+        private static async Task<Response> GenerateTokenAsync()
+        {
+            string HTMLResult;
+            JObject response;
+            var request = new
+            {
+                user = new
+                {
+                    id = "",
+                    name = ""
+                },
+            };
+            var requestJson = JsonConvert.SerializeObject(request);
+            String URL_GENERATE_TOKEN = $"{Constants.URL_GENERATE_TOKEN}";
+            WebClient wc = new WebClient();
+            wc.Headers.Add(HttpRequestHeader.Authorization, $"Bearer {Constants.DIRECT_LINE_SECRET}");
+            wc.Headers.Add(HttpRequestHeader.ContentType, Constants.APPLICATION_JSON);
+            try
+            {
+                HTMLResult = await wc.UploadStringTaskAsync(URL_GENERATE_TOKEN, requestJson);
+                response = JObject.Parse(HTMLResult);
+                Response rsp = JsonConvert.DeserializeObject<Response>(response.ToString());
+                return rsp;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private static async Task<Response> RefreshTokenAsync(string token)
+        {
+            string HTMLResult;
+            JObject response;
+            var request = new{};
+            var requestJson = JsonConvert.SerializeObject(request);
+            String URL_REFRESH_TOKEN = $"{Constants.URL_REFRESH_TOKEN}";
+            WebClient wc = new WebClient();
+            wc.Headers.Add(HttpRequestHeader.Authorization, $"Bearer {token}");
+            wc.Headers.Add(HttpRequestHeader.ContentType, Constants.APPLICATION_JSON);
+            try
+            {
+                HTMLResult = await wc.UploadStringTaskAsync(URL_REFRESH_TOKEN, requestJson);
+                response = JObject.Parse(HTMLResult);
+                Response rsp = JsonConvert.DeserializeObject<Response>(response.ToString());
+                return rsp;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
