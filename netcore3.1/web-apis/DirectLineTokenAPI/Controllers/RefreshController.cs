80ec35f106bf9886bc6147462df7cfa085b01cc9
jbenatem <jbenatem@gmail.com>
2021-03-17 17:37:36 -0500

    default controller added, pending refresh token service

--- netcore3.1/web-apis/DirectLineTokenAPI/Controllers/RefreshController.cs
+++ netcore3.1/web-apis/DirectLineTokenAPI/Controllers/RefreshController.cs
@@ -6,39 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace DirectLineTokenAPI.Controllers
 {
-    [Route("api/token/generate")]
+    [Route("api/token/refresh")]
     [ApiController]
-    public class TokenController : ControllerBase
+    public class RefreshController : ControllerBase
     {
         // POST api/<TokenController>
-        [HttpPost]
-        public async Task<Response> GenerateTokenController()
+        [HttpPost("{token}")]
+        public async Task<Response> RefreshTokenController(string token)
         {
             string HTMLResult;
             JObject response;
-            var request = new
-            {
-                user = new
-                {
-                    id = $"dl_{Guid.NewGuid()}"
-                },
-                //trustedOrigins = new object[]
-                //{
-                //    "INSERT_TRUSTED_DOMAINS"
-                //}
-            };
+            var request = new { };
             var requestJson = JsonConvert.SerializeObject(request);
-            String URL_GENERATE_TOKEN = $"{Constants.URL_GENERATE_TOKEN}";
+            String URL_REFRESH_TOKEN = $"{Constants.URL_REFRESH_TOKEN}";
             WebClient wc = new WebClient();
-            wc.Headers.Add(HttpRequestHeader.Authorization, $"Bearer {Constants.DIRECT_LINE_SECRET}");
+            wc.Headers.Add(HttpRequestHeader.Authorization, $"Bearer {token}");
             wc.Headers.Add(HttpRequestHeader.ContentType, Constants.APPLICATION_JSON);
             try
             {
-                HTMLResult = await wc.UploadStringTaskAsync(URL_GENERATE_TOKEN, requestJson);
+                HTMLResult = await wc.UploadStringTaskAsync(URL_REFRESH_TOKEN, requestJson);
                 response = JObject.Parse(HTMLResult);
                 Response rsp = JsonConvert.DeserializeObject<Response>(response.ToString());
                 return rsp;
